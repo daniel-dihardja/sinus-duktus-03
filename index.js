1a5751ccc88b164d9d4fcc389de31c192b5d13a0
daniel-dihardja <daniel@dihardja.de>
2019-10-31 21:19:09 +0100

    ...

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ import { Wave } from "./sinus-duktus/generators/wave";
 import { RandomSize } from "./sinus-duktus/modulators";
 import { CanvasRenderer } from "./sinus-duktus/renderers";
 import { UI } from './sinus-duktus/ui.js';
-import * as dat from 'dat.gui';
 import {Setup} from "./sinus-duktus/setup";
 
 const w = {
@@ -17,13 +16,7 @@ const w = {
   a: 70
 };
 
-const gui = new dat.GUI();
-const wd = gui.addFolder('wave');
 
-wd.add(w, 'd1', 0.1, 1).onChange(e => render());
-wd.add(w, 'd2', 0.1, 1).onChange(e => render());
-wd.add(w, 'o', 0.1, 1).onChange(e => render());
-wd.add(w, 'a', 0.1, 100).onChange(e => render());
 
 const wav = new Wave();
 wav.onChange = render;
@@ -36,7 +29,6 @@ rs.onChange = render;
 new UI('ui')
   .add('wave')
   .add('random size', rs.ui());
-
 
 
 
